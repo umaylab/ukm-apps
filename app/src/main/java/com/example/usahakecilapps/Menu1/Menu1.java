9c0ebea192c65724b18a42fb5901cce986db5547
umaylabPC <umaylab@gmail.com>
2021-04-30 13:38:04 +0700

    Hallo kotak warna merah

---
+++ app/src/main/java/com/example/usahakecilapps/Menu1/Menu1.java
@@ -0,0 +1,18 @@
+package com.example.usahakecilapps.Menu1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.usahakecilapps.R;
+
+public class Menu1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_menu1 );
+
+
+    }
+}
